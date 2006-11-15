3bf0ef2a78e4b0cfe1c99beddcc1dd5265199da4
rafik <rafik>
2006-11-15 09:24:46 +0000

    Zmiana silnika obslugujacego tabele z myISAM na InnoDB umo¿liwiajacego m.in. kaskadowe usuwanie.

--- ankieter/dokumentacja/baza danych/script.sql
+++ ankieter/dokumentacja/baza danych/script.sql
@@ -1,6 +1,6 @@
 /*
 Created		2006-10-27
-Modified		2006-10-29
+Modified		2006-11-15
 Project		
 Model		
 Company		
@@ -36,13 +36,13 @@ Create table ankiety (
 	data_rozpoczecia Datetime NOT NULL,
 	data_zakonczenia Datetime NOT NULL,
 	status Enum('nieaktywna', 'aktywna','zakonczona') NOT NULL,
- Primary Key (id_ankieta)) ENGINE = MyISAM
+ Primary Key (id_ankieta)) ENGINE = InnoDB
 AUTO_INCREMENT = 0;
 
 Create table respondenci (
 	id_respondent Mediumint UNSIGNED NOT NULL AUTO_INCREMENT,
 	e_mail Varchar(30) NOT NULL,
- Primary Key (id_respondent)) ENGINE = MyISAM
+ Primary Key (id_respondent)) ENGINE = InnoDB
 AUTO_INCREMENT = 0;
 
 Create table pytania (
@@ -51,7 +51,7 @@ Create table pytania (
 	id_ankieta Mediumint UNSIGNED NOT NULL,
 	kolejnosc Smallint UNSIGNED NOT NULL,
 	pytanie Text CHARACTER SET latin2 NOT NULL,
- Primary Key (id_pytanie)) ENGINE = MyISAM
+ Primary Key (id_pytanie)) ENGINE = InnoDB
 AUTO_INCREMENT = 0;
 
 Create table typy_odpowiedzi (
@@ -59,14 +59,14 @@ Create table typy_odpowiedzi (
 	nazwa Varchar(50) NOT NULL,
 	opis Varchar(250) CHARACTER SET latin2,
 	UNIQUE (nazwa),
- Primary Key (id_typ_odpowiedzi)) ENGINE = MyISAM;
+ Primary Key (id_typ_odpowiedzi)) ENGINE = InnoDB;
 
 Create table warianty_odpowiedzi (
 	id_wariant_odpowiedzi Int UNSIGNED NOT NULL AUTO_INCREMENT,
 	id_pytanie Int UNSIGNED NOT NULL,
 	kolejnosc Smallint UNSIGNED NOT NULL,
 	opis Varchar(250) CHARACTER SET latin2 NOT NULL,
- Primary Key (id_wariant_odpowiedzi)) ENGINE = MyISAM
+ Primary Key (id_wariant_odpowiedzi)) ENGINE = InnoDB
 AUTO_INCREMENT = 0;
 
 Create table odpowiedzi (
@@ -75,7 +75,7 @@ Create table odpowiedzi (
 	id_wypelniona_ankieta Int UNSIGNED NOT NULL,
 	odpowiedz Varchar(250) CHARACTER SET latin2 NOT NULL,
 	kolejnosc Smallint NOT NULL,
- Primary Key (id_odpowiedz)) ENGINE = MyISAM
+ Primary Key (id_odpowiedz)) ENGINE = InnoDB
 AUTO_INCREMENT = 0;
 
 Create table uzytkownicy (
@@ -84,13 +84,13 @@ Create table uzytkownicy (
 	haslo Varchar(15) NOT NULL,
 	grupa Tinyint NOT NULL,
 	UNIQUE (login),
- Primary Key (id_uzytkownik)) ENGINE = MyISAM
+ Primary Key (id_uzytkownik)) ENGINE = InnoDB
 AUTO_INCREMENT = 0;
 
 Create table wypelnione_ankiety (
 	id_wypelniona_ankieta Int UNSIGNED NOT NULL AUTO_INCREMENT,
 	id_ankieta Mediumint UNSIGNED NOT NULL,
- Primary Key (id_wypelniona_ankieta)) ENGINE = MyISAM;
+ Primary Key (id_wypelniona_ankieta)) ENGINE = InnoDB;
 
 
 
@@ -103,13 +103,13 @@ Create table wypelnione_ankiety (
 
 
 
-Alter table pytania add Foreign Key (id_ankieta) references ankiety (id_ankieta) on delete  restrict on update  restrict;
-Alter table wypelnione_ankiety add Foreign Key (id_ankieta) references ankiety (id_ankieta) on delete  restrict on update  restrict;
-Alter table warianty_odpowiedzi add Foreign Key (id_pytanie) references pytania (id_pytanie) on delete  restrict on update  restrict;
-Alter table odpowiedzi add Foreign Key (id_pytanie) references pytania (id_pytanie) on delete  restrict on update  restrict;
-Alter table pytania add Foreign Key (id_typ_odpowiedzi) references typy_odpowiedzi (id_typ_odpowiedzi) on delete  restrict on update  restrict;
-Alter table ankiety add Foreign Key (id_uzytkownik) references uzytkownicy (id_uzytkownik) on delete  restrict on update  restrict;
-Alter table odpowiedzi add Foreign Key (id_wypelniona_ankieta) references wypelnione_ankiety (id_wypelniona_ankieta) on delete  restrict on update  restrict;
+Alter table pytania add Foreign Key (id_ankieta) references ankiety (id_ankieta) on delete cascade on update cascade;
+Alter table wypelnione_ankiety add Foreign Key (id_ankieta) references ankiety (id_ankieta) on delete cascade on update cascade;
+Alter table warianty_odpowiedzi add Foreign Key (id_pytanie) references pytania (id_pytanie) on delete cascade on update cascade;
+Alter table odpowiedzi add Foreign Key (id_pytanie) references pytania (id_pytanie) on delete cascade on update cascade;
+Alter table pytania add Foreign Key (id_typ_odpowiedzi) references typy_odpowiedzi (id_typ_odpowiedzi) on delete cascade on update cascade;
+Alter table ankiety add Foreign Key (id_uzytkownik) references uzytkownicy (id_uzytkownik) on delete cascade on update cascade;
+Alter table odpowiedzi add Foreign Key (id_wypelniona_ankieta) references wypelnione_ankiety (id_wypelniona_ankieta) on delete cascade on update cascade;
 
 
 
