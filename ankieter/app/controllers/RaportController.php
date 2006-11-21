7ad22e122293b76751700f619fb42129e404c364
rafik <rafik>
2006-11-21 22:12:21 +0000

    *** empty log message ***

--- ankieter/app/controllers/RaportController.php
+++ ankieter/app/controllers/RaportController.php
@@ -1,5 +1,9 @@
 <?php
 include '../app/models/Ankiety.php';
+include '../app/models/Pytania.php';
+include '../app/models/Raporty.php';
+
+
 class RaportController extends Hamster_Controller_Action 
 {
     /**
@@ -20,7 +24,8 @@ class RaportController extends Hamster_Controller_Action
      */
     public function graficznyAction()
     {   
-       
+    	
+     	$this->view->body=$this->view->render('/raport/raportGraficzny.php');
         $this->display();
     }
     /**
@@ -29,8 +34,42 @@ class RaportController extends Hamster_Controller_Action
      */
     public function tabelarycznyAction()
     {   
-         $this->display();
+		$post = new Zend_Filter_Input($_POST);
+
+		$poll = new Ankiety;
+		$db = $poll->getAdapter();
+		$pollId = $this->_getParam('ankieta');
+		if (empty($pollId))$pollId = $post->getInt('ankieta_id');
+		
+		$pytanie=false;
+		$queID=$this->_getParam('pytanie');
+		if  (empty($queId)) $queId = $post->getInt('pytanie_id');
+		if (!empty($queId)) {$this->view->queId=$queId; $pytanie=true;}
+		
+		$this->view->pollId = $pollId;
+		$this->view->pool = $poll->find($pollId);
+		$view->view->questions = array(); //
+		
+		$question = new Pytania();
+		$this->view->questions = $question->findAllWithAnkietaId($pollId);
+	
+		$this->view->qV = array('jednokrotne'=>0, 'wielokrotne'=>1,'otwarte'=>2);
+		
+    	
+    	$rap=new Raporty; 
+    	$this->view->info=$rap->FindInformationsAboutAnkietaId($pollId);
+    	$this->view->fill=$rap->AmountOfFilledId($pollId);
+    	$this->view->ques=$rap->AmountOfQuestionsId($pollId);
+    	$this->view->body=$this->view->render('/raport/raportTabelaryczny.php');
+    	if ($pytanie) { 
+    		$this->view->queInfo=$rap->InfoAboutQuestionId($queId);
+    		$this->view->ansInfo=$rap->InfoAboutAnswersId($queId);
+    		$this->view->body.=$this->view->render('/raport/raportTabelarycznyPytanie.php');
+    	}
+        $this->display();
     }
+    
+    
     /**
      * Tworzony jest w locie plik exela i wysyłany jest odpowiedni nagłówek (header)
      * do przeglądarki internetowej tak że pojawia się okienko dialogowe (ie,opera, itd)
---
+++ ankieter/app/models/Raporty.php
@@ -0,0 +1,61 @@
+<?php
+class Raporty {
+	public $db;
+	public function __construct() {
+		$this->db=Zend::registry('db');
+	}
+	public function FindQuestionsWithAnkietaId($id) {
+
+		$select=$this->db->select();
+		$select->from('pytania','id_pytanie, pytanie, kolejnosc');
+		$select->where('id_ankieta=?',$id);
+		$select->order('kolejnosc');
+		$sql=$select->__toString();
+		//$query="Select pytanie from pytania";
+		return $this->db->fetchAssoc($sql);
+	}
+	public function FindInformationsAboutAnkietaId($id) {
+		$sql="Select nazwa, opis, login, status, datediff(data_zakonczenia, data_rozpoczecia) as czas, data_zakonczenia as data ".
+			 "from ankiety a, uzytkownicy u ".
+			 "where u.id_uzytkownik=a.id_uzytkownik and id_ankieta=:id_ankieta";
+		return $this->db->fetchRow($sql,array('id_ankieta'=>$id));
+	}
+	public function AmountOfFilledId($id) {
+		$sql="Select count(*) fill ".
+			 "from wypelnione_ankiety ".
+			 "where id_ankieta=:id_ankieta";
+		return $this->db->fetchRow($sql,array('id_ankieta'=>$id));
+	}
+	
+	public function AmountOfQuestionsId($id) {
+		$sql="	Select t.id_typ_odpowiedzi typ, IFNULL(p.ilosc, 0) ". 
+			"	from typy_odpowiedzi as t Left Join ".
+			"	(Select id_typ_odpowiedzi, count(*) ilosc ".
+			"	from pytania ".
+			"	where id_ankieta=:id_ankieta ".
+			"	group by id_typ_odpowiedzi) as p ".
+			"	on p.id_typ_odpowiedzi=t.id_typ_odpowiedzi";
+		return $this->db->fetchPairs($sql,array('id_ankieta'=>$id));
+	}
+	public function InfoAboutQuestionId($id) {
+		$sql="Select pytanie, typ.nazwa nazwa_typu ".
+			"from pytania, typy_odpowiedzi typ ".
+			"where pytania.id_typ_odpowiedzi=typ.id_typ_odpowiedzi and id_pytanie=:id_pytanie ";
+		return $this->db->fetchRow($sql,array('id_pytanie'=>$id));
+	}
+	
+	public function InfoAboutAnswersId($id) {
+		$sql="Select w.kolejnosc, w.opis, IFNULL(o.total,0) ilosc ".
+			"From warianty_odpowiedzi As w Left Join ".
+			"    (Select id_pytanie, odpowiedz, count(*) as total ".
+			"     From odpowiedzi ".
+			"     Where id_pytanie=:id_pytanie ".
+			"     Group By odpowiedz) As o ".
+			"On w.id_wariant_odpowiedzi = o.odpowiedz ".
+			"where w.id_pytanie=:id_pytanie ".
+			"order by w.kolejnosc ";
+		return $this->db->fetchAssoc($sql,array('id_pytanie'=>$id));
+	}
+}
+
+?>
